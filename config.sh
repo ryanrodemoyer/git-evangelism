6b54f63e43c49b999926ba06c5bf3b228f306252
ryanrodemoyer <r.rodemoyer@gmail.com>
2017-11-02 15:53:17 -0600

    add config.sh

---
+++ config.sh
@@ -0,0 +1,12 @@
+# run in Git Bash via "bash config.sh"
+
+git config --global diff.tool bc
+git config --global difftool.bc.path "C:/Program Files/Beyond Compare 4/bcomp.exe"
+git config --global difftool.prompt false
+git config --global merge.tool bc
+git config --global mergetool.bc.path "C:/Program Files/Beyond Compare 4/bcomp.exe"
+git config --global core.editor "'C:/Program Files/Sublime Text 3/subl.exe' -w"
+git config --global merge.ff no
+git config --global core.autocrlf false
+git config --global merge.ff false
+git config --global pull.ff only
