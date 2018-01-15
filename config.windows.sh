ca9a28a26456a6ae65ddf013949ddb8075046b50
ryanrodemoyer <r.rodemoyer@gmail.com>
2018-01-15 09:39:07 -0700

    add comments to config script

--- config.windows.sh
+++ config.windows.sh
@@ -1,17 +1,31 @@
-# run in Git Bash via "bash config.sh"
+# run in Git Bash via "bash config.windows.sh"
 
+# initialize git lfs
 git lfs install
+
+# set a default value for credential.helper so git lfs can properly get user credentials
 git config --global credential.helper manager
+
+# make beyond compare the difftool
 git config --global diff.tool bc
 git config --global difftool.bc.path "C:/Program Files/Beyond Compare 4/bcomp.exe"
 git config --global difftool.prompt false
+
+# make beyond compare the mergetool
 git config --global merge.tool bc
 git config --global mergetool.bc.path "C:/Program Files/Beyond Compare 4/bcomp.exe"
+
+# make sublime text the default text editor
 git config --global core.editor "'C:/Program Files/Sublime Text 3/subl.exe' -w"
-git config --global merge.ff no
-git config --global core.autocrlf false
+
+# always create a merge commit, or restated as do not perform fast forward merges
 git config --global merge.ff false
+
+# during a pull, only a fast forward merge is allowed. overrides merge.ff
 git config --global pull.ff only
+
+# windows machine doing windows work
+git config --global core.autocrlf false
 
 # auto discard .orig files after a merge conflict is resolved
 git config --global mergetool.keepBackup false
